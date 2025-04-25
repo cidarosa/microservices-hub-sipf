ab41a31307c082e145bab59cedfcb56e8d63d549
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-25 10:24:15 -0300

    feat: Entity ItemDoPedido

---
+++ ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/entities/ItemDoPedido.java
@@ -0,0 +1,24 @@
+package com.github.cidarosa.ms_pedido.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_item_do_pedido")
+public class ItemDoPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+    private BigDecimal valorUnitario;
+}
