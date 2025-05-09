c72399cb275317bb7840ac559c0de4e1b034a189
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-09 09:37:16 -0300

    feat: update em PedidoService e PedidoController, add Query em ItemDoPedidRepository

--- ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/controller/PedidoController.java
+++ ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/controller/PedidoController.java
@@ -45,6 +45,14 @@ public class PedidoController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<PedidoDTO> updatePedido(@PathVariable Long id,
+                                                  @RequestBody @Valid PedidoDTO dto){
+
+        dto = service.updatePedido(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
 
 
 
