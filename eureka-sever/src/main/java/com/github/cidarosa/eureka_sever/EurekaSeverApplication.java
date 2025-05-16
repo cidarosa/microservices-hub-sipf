cb8ff860ec8d410cb4ac5dc5137c1ec776382e7a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-16 08:58:24 -0300

    feat: add Eureka Server

---
+++ eureka-sever/src/main/java/com/github/cidarosa/eureka_sever/EurekaSeverApplication.java
@@ -0,0 +1,13 @@
+package com.github.cidarosa.eureka_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSeverApplication.class, args);
+	}
+
+}
